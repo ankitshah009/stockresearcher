a0eea9b66344306702bb3dd5e5b92c6ec2256e24
Ankit Shah <ankit.tronix@gmail.com>
2025-04-12 17:11:56 -0700

    commit the port change

--- stock-researcher-backend/app.py
+++ stock-researcher-backend/app.py
@@ -128,5 +128,5 @@ def search_stock():
     return jsonify({'message': f'No stocks found matching {symbol}'}), 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=True) 
